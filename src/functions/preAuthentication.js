3d3a7f6386867d59ec3b4fdf95f4dd561087151b
xenotime <sandeepkhoj@gmail.com>
2022-10-15 20:50:44 +0530

    quick fix

--- src/functions/preAuthentication.js
+++ src/functions/preAuthentication.js
@@ -6,14 +6,20 @@ exports.handler = async (event, context, callback) => {
   const user = await lookupUser(event.request.userAttributes.email);
 
   if (user) {
-    if (user.user_status__pc !== 'Active') {
-      // Return error to Amazon Cognito
-      return callback(new Error('Account does not exist.'), event);
-    } else if (user.user_status__pc !== 'Disabled') {
+    if (user.user_status__pc === 'Disabled') {
       // Return error to Amazon Cognito
       return callback(
         new Error(
           'You have been disabled from using your account. Please contact customer service for assistance.'
+        ),
+        event
+      );
+    }
+    if (user.user_status__pc !== 'Active') {
+      // Return error to Amazon Cognito
+      return callback(
+        new Error(
+          'Account does not exist. Please contact customer service for assistance.'
         ),
         event
       );
