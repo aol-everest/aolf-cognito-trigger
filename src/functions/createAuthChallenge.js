e6a712d95be733418deaaf59ec64b84890a6d691
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-04-02 21:11:19 +0530

    test es6

--- src/functions/createAuthChallenge.js
+++ src/functions/createAuthChallenge.js
@@ -1,25 +1,49 @@
-const crypto = require('crypto');
+import * as fido2 from './../services/fido2.js';
+import * as smsOtpStepUp from './../services/sms-otp-stepup.js';
+import * as magicLink from './../services/magic-link.js';
+import { logger, UserFacingError } from './../services/common.js';
 
-exports.handler = async (event) => {
-  console.log(event);
-
-  var publicKeyCred = event.request.userAttributes['custom:publicKeyCred'];
-  var publicKeyCredJSON = Buffer.from(publicKeyCred, 'base64').toString(
-    'ascii'
-  );
-  console.log(JSON.parse(publicKeyCredJSON));
-
-  const challenge = crypto.randomBytes(64).toString('hex');
-
-  event.response.publicChallengeParameters = {
-    credId: JSON.parse(publicKeyCredJSON).id, //credetnial id
-    challenge: challenge,
-  };
-
-  event.response.privateChallengeParameters = { challenge: challenge };
-  console.log(
-    'privateChallengeParameters=' + event.response.privateChallengeParameters
-  );
-
-  return event;
+export const handler = async (event) => {
+  logger.debug(JSON.stringify(event, null, 2));
+  try {
+    if (!event.request.session || !event.request.session.length) {
+      // This is the first time Create Auth Challenge is called
+      // Create a dummy challenge, allowing the user to send a challenge response
+      // with client metadata, that can be used to to provide auth parameters:
+      // - Redirect URL for magic link
+      // - [OPTIONAL] Skip creation of new secret sign-in code (if client already has one)
+      // - Sign-in method
+      logger.info('Client has no session yet, starting one ...');
+      await provideAuthParameters(event);
+      // If enabled, fido2 challenge is attached to the event always, even if the client might want to use another signInMethod.
+      // This is so that client can immediately respond with a FIDO2 signature
+      await fido2.addChallengeToEvent(event);
+    } else {
+      const { signInMethod } = event.request.clientMetadata ?? {};
+      logger.info(`Client has requested signInMethod: ${signInMethod}`);
+      if (signInMethod === 'MAGIC_LINK') {
+        await magicLink.addChallengeToEvent(event);
+      } else if (signInMethod === 'SMS_OTP_STEPUP') {
+        await smsOtpStepUp.addChallengeToEvent(event);
+      } else {
+        throw new Error(`Unrecognized signInMethod: ${signInMethod}`);
+      }
+    }
+    logger.debug(JSON.stringify(event, null, 2));
+    return event;
+  } catch (err) {
+    logger.error(err);
+    if (err instanceof UserFacingError) throw err;
+    throw new Error('Internal Server Error');
+  }
 };
+
+async function provideAuthParameters(event) {
+  logger.info('Creating challenge: PROVIDE_AUTH_PARAMETERS');
+  event.response.challengeMetadata = 'PROVIDE_AUTH_PARAMETERS';
+  const parameters = {
+    challenge: 'PROVIDE_AUTH_PARAMETERS',
+  };
+  event.response.privateChallengeParameters = parameters;
+  event.response.publicChallengeParameters = parameters;
+}
