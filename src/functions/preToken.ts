5977e8749ded298671bfe0e3bcc196e93507c081
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-04-08 18:14:24 +0530

    fixed ts

--- src/functions/preToken.ts
+++ src/functions/preToken.ts
@@ -1,11 +1,27 @@
-import { logger, UserFacingError } from './../common.js';
+/**
+ * Copyright Amazon.com, Inc. and its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You
+ * may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+import { PreTokenGenerationTriggerHandler } from 'aws-lambda';
+import { logger, UserFacingError } from './../services/common';
 
 const CLIENT_METADATA_PERSISTED_KEYS =
   process.env.CLIENT_METADATA_PERSISTED_KEYS?.split(',').map((key) =>
     key.trim()
   ) ?? [];
 
-export const handler = async (event) => {
+export const handler: PreTokenGenerationTriggerHandler = async (event) => {
   logger.debug(JSON.stringify(event, null, 2));
   logger.info('PreToken Generation for trigger:', event.triggerSource);
   if (event.triggerSource === 'TokenGeneration_Authentication') {
@@ -27,7 +43,7 @@ export const handler = async (event) => {
         if (stringValue.length > 256) {
           throw new UserFacingError(`Value for "${key}" too long`);
         }
-        event.response.claimsOverrideDetails.claimsToAddOrOverride[
+        event.response.claimsOverrideDetails.claimsToAddOrOverride![
           snakeCase(key)
         ] = stringValue;
       });
@@ -37,6 +53,6 @@ export const handler = async (event) => {
   return event;
 };
 
-function snakeCase(s) {
+function snakeCase(s: string) {
   return s.replace(/[A-Z]{1}/g, (matched) => `_${matched.toLowerCase()}`);
 }
