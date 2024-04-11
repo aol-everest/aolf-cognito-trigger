b9b21f9003ed37bd821af9e71ab79529646ebcd3
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-04-11 20:28:00 +0530

    - added api functions

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports.CONFIG = (serverless) => ({
   DATABASE_URL: process.env.DATABASE_URL,
+  COGNITO_USER_POOL_ID: process.env.COGNITO_USER_POOL_ID,
   IAHV_CLIENT_ID: process.env.IAHV_CLIENT_ID,
   HB_CLIENT_ID: process.env.HB_CLIENT_ID,
   AOL_CLIENT_ID: process.env.AOL_CLIENT_ID,
