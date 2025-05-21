e648dd7a9afc577945696e3731e3131366ceeaed
Sandeep Kumar <sandeepkhoj@gmail.com>
2025-05-22 01:48:31 +0530

    fixed pool

--- src/services/knex.ts
+++ src/services/knex.ts
@@ -13,10 +13,17 @@ function createKnexConnection(connection: string, searchPath: string) {
     },
     searchPath,
     debug: process.env.IS_LOCAL_DEVELOPMENT === 'TRUE',
-    // pool: {
-    //   min: Number(process.env.DATABASE_POOL_MIN || '0'),
-    //   max: Number(process.env.DATABASE_POOL_MAX || '10'),
-    // },
+    pool: {
+      min: Number(process.env.DATABASE_POOL_MIN || '20'),
+      max: Number(process.env.DATABASE_POOL_MAX || '70'),
+      idleTimeoutMillis: 30000, // 30 seconds (increased from 3s)
+      acquireTimeoutMillis: 30000, // 60 second timeout when acquiring connection
+      reapIntervalMillis: 5000, // Check for idle connections every second
+      createTimeoutMillis: 15000, // 30 second timeout when creating connection
+      createRetryIntervalMillis: 500, // Retry creating connection every 200ms
+      propagateCreateError: false, // Don't crash pool on connection failure
+    },
+    acquireConnectionTimeout: 30000,
   });
 }
 
